913e13c4a2214d3d6407999a1e0efe7e858473a6
Alberto Guzman <112503817+TimmyElTaco@users.noreply.github.com>
2023-11-08 12:25:24 -0600

    Add some librarys; create data base
    We add some librarys for the backend and create a
    data base, modify the register form and login form and
    change some style

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
-from flask import render_template, request, Flask, session
+from flask import render_template, request, Flask, session, redirect
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_session import Session
+
 import sqlite3
 
+#configuration of application
 app = Flask(__name__)
+
+#configuration of session
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    return render_template("login.html")
+    if request.method == 'POST':
+        return redirect('/')
+    else:
+        return render_template("login.html")
 
-@app.route('/register')
+@app.route('/register', methods=['POST', 'GET'])
 def register():
-    return render_template("register.html")
+    if request.method == 'POST':
+        return redirect('/')
+    else:
+        return render_template("register.html")
