01924dbd635fbb2e596aa2803298952f97fe2ee3
Alberto Guzman <112503817+TimmyElTaco@users.noreply.github.com>
2023-11-08 16:25:20 -0600

    Create backend of register
    Create the connection with the database and
    the expect errors of the register form

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -26,6 +27,46 @@ def login():
 @app.route('/register', methods=['POST', 'GET'])
 def register():
     if request.method == 'POST':
-        return redirect('/')
+        name = request.form.get("name")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+        country = request.form.get("country")
+
+        if not name:
+            return render_template("register.html", error="Introduce tu nombre")
+        elif not email:
+            return render_template("register.html", error="Introduce tu correo electronico")
+        elif not country:
+            return render_template("register.html", error="Introduce tu pais")
+        elif confirmation != password or not password or not confirmation:
+            return render_template("register.html", error="Coloca una contraseña valida")
+        password = generate_password_hash(password)
+
+        try:
+            con = sqlite3.connect('edad-de-oro.db')
+            cur = con.cursor()
+
+            try:
+                cur.execute('INSERT INTO users(name, email, hash, country) VALUES (?, ?, ?, ?);', (name, email, password, country))
+
+            
+            
+            except Exception as e:
+                print(f"Error en la edicion de la base de datos: {str(e)}")
+            
+            finally:
+                cur.close()
+        
+        except Exception as e:
+            print(f"Error en creacion de conexion y cursor: {str(e)}")
+        finally:
+            con.commit()
+            con.close()
+            
+        return redirect('/login')
     else:
         return render_template("register.html")
+    
+if __name__ == '__main__':
+    app.run()
